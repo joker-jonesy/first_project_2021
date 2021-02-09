be2bd8d5f898b5313e56bdd1a1a23c4c5ac6d520
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2021-02-09 16:09:51 -0500

    Name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Luke.
+          Hello my name is Henry.
         </p>
         <a
           className="App-link"
